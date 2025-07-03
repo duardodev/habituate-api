3580e1bb4e80db9a31aafe8a3c32c2c1b358b34e
Deivit Eduardo <deiviteduardo87@gmail.com>
2025-07-03 14:13:32 -0300

    refactor: update health check endpoint to use HEAD method

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 
 export async function healthCheck(app: FastifyInstance) {
-    app.get("/health", async (_, reply) => {
-        return reply.status(200).send("OK");
+    app.head("/health", async (_, reply) => {
+        return reply.status(200).send();
     })
 }
